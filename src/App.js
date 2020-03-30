966dc814c652de88a06d859d429ff76d2ffa2e3b
wilder <er.feltesse@gmail.com>
2020-03-30 14:14:09 +0200

    ajout des quote cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar' ;
-
+import QuoteCard from './components/QuoteCard' ;
 
 function App() {
   return (
     <div className="App" >
  <Navbar/>
+ <QuoteCard/>
+ <QuoteCard />
     </div>
   );
 }
---
+++ src/components/QuoteCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './QuoteCard.css';
+function QuoteCard () {
+return (  <figure class="QuoteCard">
+<img
+  src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
+  alt="Nelson Muntz"/>
+<figcaption>
+  <blockquote>
+    Shoplifting is a victimless crime, like punching someone in the dark.
+  </blockquote>
+  <cite>Nelson Muntz</cite>
+</figcaption>
+</figure>
+
+)}
+
+export default QuoteCard;
