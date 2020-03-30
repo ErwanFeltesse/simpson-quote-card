2d5b910237e57fb418f90723d129add841092d25
wilder <er.feltesse@gmail.com>
2020-03-30 12:18:58 +0200

    ajout de la Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Navbar from './components/Navbar' ;
 
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App" >
+ <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+    return (<nav>
+<a href="https://fr.wikipedia.org/wiki/Les_Simpson">Simpsons Quotes</a>
+
+    </nav>)
+}
+
+
+
+
+export default Navbar;
